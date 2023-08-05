0d35d3d4952ffca83a8fbf631a128bf68e8f1242
luislourenco <luislourenco18@gmail.com>
2023-08-05 12:36:40 -0300

    adicona e exibi filmes

---
+++ src/main/java/br/com/alura/screenmatch/controller/FilmeController.java
@@ -0,0 +1,48 @@
+package br.com.alura.screenmatch.controller;
+
+import br.com.alura.screenmatch.domain.filme.DadosCadastroFilme;
+import br.com.alura.screenmatch.domain.filme.Filme;
+import br.com.alura.screenmatch.domain.filme.FilmeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("/filmes")
+public class FilmeController {
+
+    @Autowired
+    private FilmeRepository repository;
+
+    @GetMapping("/formulario")
+    public String carregaPaginaFormulario(){
+        return "filmes/formulario";
+
+    }
+
+
+    @GetMapping
+    public String carregaPaginaListagem(Model model){
+        model.addAttribute("lista", repository.findAll());
+        return "filmes/listagem";
+
+    }
+    @PostMapping
+    public String cadastraFilme(DadosCadastroFilme dados){
+
+       // System.out.println(dados);
+        var filme = new Filme(dados);
+        repository.save(filme);
+       // System.out.println(filmes);
+
+       return "redirect:/filmes";
+
+    }
+
+}
---
+++ src/main/java/br/com/alura/screenmatch/domain/filme/DadosCadastroFilme.java
@@ -0,0 +1,6 @@
+package br.com.alura.screenmatch.domain.filme;
+
+
+public record DadosCadastroFilme(String nome, Integer duracao, Integer ano, String genero) {
+
+}
